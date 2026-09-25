2d9589594825ac9b996c6bb74b2518407f32a3e6
janalibaba16-cpu <janalibaba16@gmail.com>
2026-09-25 12:10:08 +0500

    Create stream.ts

---
+++ providers/crunchyroll/stream.ts
@@ -0,0 +1,8 @@
+export const getStream = async function ({ link, type, providerContext }) {
+  return [
+    {
+      title: "Crunchyroll Server",
+      url: "https://crunchyroll.com"
+    }
+  ];
+};

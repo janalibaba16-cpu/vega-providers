df3334e43d8b6338f2908a03cd01af6298e35b96
janalibaba16-cpu <janalibaba16@gmail.com>
2026-09-25 12:09:20 +0500

    Create meta.ts

---
+++ providers/crunchyroll/meta.ts
@@ -0,0 +1,9 @@
+export const getMeta = async function ({ link, providerContext }) {
+  return {
+    title: "Anime Movie",
+    synopsis: "Streaming directly from Crunchyroll",
+    image: "",
+    type: "movie",
+    linkList: []
+  };
+};
